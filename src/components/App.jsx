7426d6074506f792fe6496b91ae1c61b58e24c9a
ArtemYakushkin <zimberger29101985@gmail.com>
2022-08-06 14:56:24 +0300

    add FriendList

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,8 @@ import { Profile } from "./Profile/Profile";
 import user from "./Profile/user";
 import { Statistics } from "./Statistics/Statistics";
 import data from "./Statistics/data"
+import { FriendList } from "./Friends/FriendList";
+import friends from "./Friends/friends";
 
 export const App = () => {
   return (
@@ -9,6 +11,7 @@ export const App = () => {
       <Profile user={user} />
       <Statistics title="Upload stats" stats={data} />
       {/* <Statistics stats={data} /> */}
+      <FriendList friends={friends} />
     </div>
   );
 };
---
+++ src/components/Friends/FriendList.styled.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const List = styled.ul`
+    margin-bottom: 20px;
+    list-style: none;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 0px;
+`;
+
+export const ListItem = styled.li`
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    width: 250px;
+    border: 1px solid #4E555F;
+    border-radius: 4px;
+    margin-bottom: 8px;
+    padding: 8px;
+`;
